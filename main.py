c44432b0ad26e4f388845627a4254bad9e584113
ogomegbunam <58264605+ogomegbunam@users.noreply.github.com>
2023-04-28 07:48:05 +0100

    adding schema to the api

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 from fastapi import FastAPI
+
+
 from fastapi.params import Body
+
+
 from pydantic import BaseModel
 
+from typing import Optional
+
+
 app = FastAPI()
+
+
 
 class Post(BaseModel):
     title: str
     content: str
-    isPublished: bool
+
+
+    isPublished: bool = True
+
+    rating: Optional[int] = None
+
+
 
 @app.get("/")
+
+
 async def root():
+
+
     return {"message": "Welcome to my api"}
 
 
+
+
 @app.get("/get_post")
+
+
 def get_post():
+
+
     return {"data": "This is your post"}
+
+
 @app.post("/createpost")
+
+
 def create_post(post: Post):
-    print(payload)
-    return{"new_post":f"title{payload['title']} content:{payload['content']}"}
+    print(post.dict())
+
+    return{"data":post}
